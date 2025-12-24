4f6087440803837171c03b2da05d89e4b092477e
Stelios Kiliaris <stylianosk7@gmail.com>
2025-12-24 12:46:39 +0200

    finished lesson 1

--- Lesson1/module1.cs
+++ Lesson1/module1.cs
@@ -1,1 +1,11 @@
-Console.WriteLine("Hello World!");
+//Console.WriteLine("Hello World");
+
+//Console.Write("Congratulations!");
+//Console.Write(" ");
+//Console.Write("You wrote your first lines of code.");
+
+//Console.WriteLine("Congratulations!");
+//Console.Write("You wrote your first lie of code.");
+
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
---
+++ Lesson1/module4.cs
@@ -0,0 +1,53 @@
+//int firstNumber = 12;
+//int secondNumber = 7;
+//Console.WriteLine(firstNumber + secondNumber);
+
+//string firstName = "Bob";
+//int widgetsSold = 7;
+//Console.WriteLine(firstName + " sold " + widgetsSold + " 7" + " widgets.");
+
+
+/**
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+*/
+
+/**
+int value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value--;
+Console.WriteLine("Third decrement: " + value);
+*/
+
+//BECAUSE DECIMALS NEED TO USE M
+int farenheit = 94;
+decimal celcius = (farenheit - 32m);
+celcius = celcius * (5m/9m);
+
+Console.WriteLine("The temperature is " + celcius + " Celsius.");
+
+int result = 3 + 1 * 5 / 2;
+Console.WriteLine(result);
+Console.WriteLine(5 / 10);
