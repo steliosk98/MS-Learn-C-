a85c4e190f067888a7fc51b42eab8f26832bbbe1
Stelios Kiliaris <stylianosk7@gmail.com>
2025-12-24 11:47:00 +0200

    finished first 3 modules of course

---
+++ Lesson1/module2.cs
@@ -0,0 +1,46 @@
+//Console.WriteLine('b');
+
+//Console.WriteLine('Hello World!');
+
+//Integer
+//Console.WriteLine(123);
+
+//Float
+//Console.WriteLine(0.25F);
+
+//Double
+//Console.WriteLine(2.625);
+
+//Decimal
+//Console.WriteLine(12.39816m);
+
+//Console.WriteLine(true);
+//Console.WriteLine(false);
+
+/**
+string firstName;
+
+char userOption;
+
+int gameScore;
+
+decimal particlesPerMillion;
+
+bool processedCustomer;
+
+*/
+
+//string firstName;
+//firstName = "Bob";
+//Console.WriteLine(firstName);
+
+//string firstName = "Bob";
+//Console.WriteLine(firstName);
+
+//Exercise
+string name = "Bob";
+int number = 3;
+double d = 34.4;
+
+Console.WriteLine("Hello, " + name +"! You have " + number + " messages in your inbox. The temperature is " + d + " celsius.");
+Console.WriteLine(34.40M);
---
+++ Lesson1/module3.cs
@@ -0,0 +1,51 @@
+//Console.WriteLine("Hello \"World\"!");
+
+//Console.WriteLine("c:\\source\\repos");
+
+/**
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.Write("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+
+Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
+*/
+
+// Kon'nichiwa World
+//Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+
+
+/**
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.Write("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// User command to run an application
+Console.WriteLine(@"c:\invoices\app.exe -j");
+
+*/
+
+/**
+int version = 11;
+string updateText = "Update to Windows";
+string message = $"{updateText} {version}";
+Console.WriteLine(message);
+
+*/
+
+//Exercise
+string projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine("View English output:\n  " + $"c:\\Exercise\\{projectName}\\data.txt");
+Console.WriteLine($"{russianMessage}:\n  " + $"c:\\Exercise\\{projectName}\\ru-RU\\data.txt");
+
+
