747b7f1697b2e30c175b513d399d29d4c34f8b57
Stelios Kiliaris <stylianosk7@gmail.com>
2025-12-24 14:06:25 +0200

    finished lesson2

---
+++ Lesson2/Module3/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,129 @@
+﻿/**
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you lose.");
+}
+*/
+
+/**
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+
+if ((roll1 == roll2) && (roll2 == roll3)) 
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you lose.");
+}
+*/
+
+/**
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+
+if ((roll1 == roll2) && (roll2 == roll3)) 
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+
+else
+{
+    Console.WriteLine("Sorry, you lose.");
+} */
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+Console.WriteLine($"Days until subscription expiration: {daysUntilExpiration}");
+
+// Your code goes here
+if (daysUntilExpiration <= 10)
+{
+    if (daysUntilExpiration <= 5)
+    {
+        if (daysUntilExpiration <= 1)
+        {
+            if (daysUntilExpiration == 0)
+            {
+                Console.WriteLine("Your subscription has expired!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Your subscription will expire soon. Renew now!");
+    }
+}
---
+++ Lesson2/Module5/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,49 @@
+﻿/**
+string firstName = "Bob";
+int widgetsPurchased = 7;
+// Testing a change to the message.
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+*/
+
+/**
+Random random = new Random();
+string[] orderIDs = new string[5];
+// Loop through each blank orderID
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    // Get a random value that equates to ASCII letters A through E
+    int prefixValue = random.Next(65, 70);
+    // Convert the random value into a char, then a string
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    // Create a random number, pad with zeroes
+    string suffix = random.Next(1, 1000).ToString("000");
+    // Combine the prefix and suffix together, then assign to current OrderID
+    orderIDs[i] = prefix + suffix;
+}
+// Print out each orderID
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
+}
+*/
+
+string str = "The quick brown fox jumps over the lazy dog.";
+
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+
+// Reverse the characters
+Array.Reverse(charMessage);
+int x = 0;
+
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+
+// convert it back to a string
+string new_message = new String(charMessage);
+
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
